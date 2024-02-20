6c1777aa84d0957d46c416d7148926c9336f8a6e
Harshit <harshit.it.21@nitj.ac.in>
2024-02-20 13:35:32 +0530

    Done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -13,7 +13,7 @@ const Intro = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={img} alt="Rasif Taghizade" />
+            <img src={img} alt="Harshit Sharma" />
           </div>
         </div>
         <div className="about__content">
